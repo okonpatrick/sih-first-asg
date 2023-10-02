99bb37bc131012b38f0206480f159ea6ef66f7f3
okonpatrick <okonpatrick2000@gmail.com>
2023-10-02 07:24:30 +0100

    SIH assignment

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'font-awesome/css/font-awesome.min.css';
 import Navbar from './components/NavBar'
 import WelcomePage from './components/WelcomePage'
 import ContentPage from './components/ContentPage'
